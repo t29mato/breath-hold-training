fe0413c70c1894b4913ec50ecc46ced2e092d932
mato <tomoya.matou@gmail.com>
2018-10-20 23:08:23 +0900

    タイマーが動くようになった

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import Training from './components/Training'
 import Result from './components/Result'
 import Tutorial from './components/Tutorial'
 import News from './components/News'
+import Header from './components/Header'
 
 class App extends Component {
   render() {
     return(
       <BrowserRouter>      
         <div>
+          <Header/>
           <Route exact path='/' component={Training} />
           <Route path='/result' component={Result} />
           <Route path='/tutorial' component={Tutorial} />
