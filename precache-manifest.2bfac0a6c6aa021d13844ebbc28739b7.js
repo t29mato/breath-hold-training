0148972f57c2b933401fa00f314fa519ba90a1ad
mato <tomoya.matou@gmail.com>
2018-10-23 23:11:11 +0900

    Updates

--- precache-manifest.2bfac0a6c6aa021d13844ebbc28739b7.js
+++ precache-manifest.2bfac0a6c6aa021d13844ebbc28739b7.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/breath-hold-training/static/js/runtime~main.ce263281.js"
   },
   {
-    "revision": "9ca67bf88c3a6d9b69a0",
-    "url": "/breath-hold-training/static/js/main.9ca67bf8.chunk.js"
+    "revision": "96a1c1dfe30343a0754f",
+    "url": "/breath-hold-training/static/js/main.96a1c1df.chunk.js"
   },
   {
     "revision": "e4017aa80a1a4fa1dbdd",
     "url": "/breath-hold-training/static/js/1.e4017aa8.chunk.js"
   },
   {
-    "revision": "919aa87575c1eb8b7b3d039fd1e085ae",
+    "revision": "2579462632134b28ff52369975b6f5c8",
     "url": "/breath-hold-training/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/breath-hold-training/precache-manifest.c33b3c088551b423a7adc82e40fa4d51.js"
+  "/breath-hold-training/precache-manifest.2bfac0a6c6aa021d13844ebbc28739b7.js"
 );
 
 workbox.clientsClaim();
